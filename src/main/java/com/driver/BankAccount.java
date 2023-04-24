28ed12e2c6063b6d374b6d649ec877c02e402658
Parteekpal Singh <sparteek9@gmail.com>
2023-04-24 19:54:41 +0530

    third commit

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -30,34 +30,41 @@ public class BankAccount {
         return balance;
     }
 
-    public String getAccountNum(int digits, int sum, String res){
-
-        if(digits<0 || sum<0){
-            return "";
-        }
+    public boolean getAccountNum(int digits, int sum, String temp,StringBuilder res){
 
         if(digits==0 && sum==0){
-            return res;
+
+            res.append(temp);
+            return true;
         }
-        String temp="";
+
+        if(digits<=0 || sum<0){
+            return false;
+        }
+
+       boolean check=false;
         for(int i=0;i<=9;i++){
-            res+="" + ((char)('0'+i));
-            temp=getAccountNum(digits-1,sum-i,res);
-            if(temp.length()!=0){
-                return temp;
+
+
+            check=check || getAccountNum(digits-1,sum-i,temp+ ((char)('0'+i)),res);
+            if(check){
+                return true;
             }
+
         }
-        return temp;
+
+        return check;
 
     }
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-
-        String res=getAccountNum(digits,sum,new String(""));
+        StringBuilder res=new StringBuilder();
+        getAccountNum(digits,sum,new String(""),res);
+        String result=res.toString();
         if(res.length()!=0){
-            return res;
+            return result;
         }else{
             throw new Exception("Account Number can not be generated");
         }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,5 +3,13 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
 
+//        BankAccount b1=new BankAccount("Parteke",1000,400);
+//        try{
+//            System.out.println(b1.generateAccountNumber(4,28));
+//        }catch (Exception e){
+//            System.out.println("Error");
+//        }
+
+
     }
 }
